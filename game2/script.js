67b9ab3e5fbcb445564ab70f515c9e469787850a
Amol7Developer <amol0783com@gmail.com>
2023-05-10 12:49:46 +0530

    code updated for one click for four action

--- game2/script.js
+++ game2/script.js
@@ -1,6 +1,9 @@
 function myMove() {
     let id = null;
-    const elem = document.getElementById("animate");   
+    const elem = document.getElementById("animate");  
+    const elem1 = document.getElementById("animate1"); 
+    const elem2 = document.getElementById("animate2");  
+    const elem3 = document.getElementById("animate3");   
     let pos = 0;
     clearInterval(id);
     id = setInterval(frame, 5);
@@ -11,6 +14,12 @@ function myMove() {
         pos++; 
         elem.style.top = pos + "px"; 
         elem.style.left = pos + "px"; 
+        elem1.style.top = pos + "px"; 
+        elem1.style.left = pos + "px"; 
+        elem2.style.top = pos + "px"; 
+        elem2.style.left = pos + "px"; 
+        elem3.style.top = pos + "px"; 
+        elem3.style.left = pos + "px"; 
       }
     }
   }
